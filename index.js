d5260733a294a16b3d8bd9cca3fadb707bae6ff0
Abel <abelguevaralanda@yahoo.es>
2023-01-30 09:00:28 +0100

    fix error return of function

--- index.js
+++ index.js
@@ -9,34 +9,25 @@ const loopArray = () => {
   let countNeighbor = 0;
   for (let x = 0; x < lifeArray.length; x++) {
     for (let y = 0; y < lifeArray.length; y++) {
-      //1
       if (x > 0 && y > 0) if (lifeArray[x - 1][y - 1]) countNeighbor++;
 
-      //2
       if (x > 0) if (lifeArray[x - 1][y]) countNeighbor++;
 
-      //3
       if (x > 0 && y < lifeArray.length - 1)
         if (lifeArray[x - 1][y + 1]) countNeighbor++;
 
-      //4
       if (y > 0) if (lifeArray[x][y - 1]) countNeighbor++;
 
-      //5
       if (y < lifeArray.length - 1) if (lifeArray[x][y + 1]) countNeighbor++;
 
-      //6
       if (x < lifeArray.length - 1 && y > 0)
         if (lifeArray[x + 1][y - 1]) countNeighbor++;
 
-      //7
       if (x < lifeArray.length - 1) if (lifeArray[x + 1][y]) countNeighbor++;
 
-      //8
       if (x < lifeArray.length - 1 && y < lifeArray.length - 1)
         if (lifeArray[x + 1][y + 1]) countNeighbor++;
 
-      //reglas
       if (lifeArray[x][y] === false && countNeighbor > 3)
         lifeArray[x][y] = true;
 
@@ -53,12 +44,11 @@ const loopArray = () => {
         lifeArray[x][y] = false;
 
       console.log(lifeArray[x][y]);
-
-      //newLifeArray.push(lifeArray);
     }
   }
   console.log(countNeighbor);
   newLifeArray.push(lifeArray);
+  return console.log(lifeArray);
 };
 
 console.log(loopArray());
